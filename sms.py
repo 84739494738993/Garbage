61b431cb7f0dba66293b8c5dc8184cf1aab45560
unknown <maxn141209@gmail.com>
2026-01-11 12:00:23 +0100

    Initial commit

--- sms.py
+++ sms.py
@@ -11,10 +11,9 @@ def get_date():
     list_day_bioodpady   = [6,20,6,20,3,17,26,15,29,12,26,10,24,7,21,4,18,2,16,30,13,27,11,20,8,22]
     list_day_gabaryty   = [3,16]
     list_month_zmieszane   = [2,2,3,3,4,4,5,5,5,6,6,7,7,8,8,9,9,10,10,10,11,11,12,12,1,1]
-    list_month_segregacja   = [2,3,4,5,6,7,8,9,10,11,12,1]
+    list_month_segregacja   = [2,3,4,5,6,7,8,9,10,11,12,12,1]
     list_month_bioodpady   = [2,2,3,3,4,4,4,5,5,6,6,7,7,8,8,9,9,10,10,10,11,11,12,12,1,1]
     list_month_gabaryty   = [4,10]
-
     today = date.today()
     now = datetime.now()
     day = today.day
